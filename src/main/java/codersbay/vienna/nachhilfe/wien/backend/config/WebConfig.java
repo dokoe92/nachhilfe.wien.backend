8ac56da337b0995f6f48a39ee7c6cdc129582ca1
Dominik Köberl <dokoe92@gmail.com>
2023-06-07 15:03:11 +0200

    added comment in webconfig

--- src/main/java/codersbay/vienna/nachhilfe/wien/backend/config/WebConfig.java
+++ src/main/java/codersbay/vienna/nachhilfe/wien/backend/config/WebConfig.java
@@ -7,6 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * Configures Cross-Origin Resource Sharing (CORS) for the application.
+     * Allows requests from any origin, with any method and headers.
+     *
+     * MUST BE FIXED!!! ORIGINS MUST BE CAREFULLY DEFINED DUE TO SAFETY REASONS
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
---
+++ src/main/java/codersbay/vienna/nachhilfe/wien/backend/respository/FeedbackRepository.java
@@ -0,0 +1,7 @@
+package codersbay.vienna.nachhilfe.wien.backend.respository;
+
+import codersbay.vienna.nachhilfe.wien.backend.model.Entity.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+}
