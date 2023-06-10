4985b3e0d825459b2571a8636d8abc54dbf884e4
René-Ali Günürün <renee.guenueruen@gmail.com>
2023-06-10 14:34:51 +0200

    updating Admin Firstname && Lastname && Description possible | status, pw and email WIP

--- src/main/java/codersbay/vienna/nachhilfe/wien/backend/rest/AdminController.java
+++ src/main/java/codersbay/vienna/nachhilfe/wien/backend/rest/AdminController.java
@@ -1,15 +1,21 @@
 package codersbay.vienna.nachhilfe.wien.backend.rest;
 
 import codersbay.vienna.nachhilfe.wien.backend.model.Entity.Admin;
+import codersbay.vienna.nachhilfe.wien.backend.model.Entity.Profile;
+import codersbay.vienna.nachhilfe.wien.backend.model.Entity.Student;
+import codersbay.vienna.nachhilfe.wien.backend.model.Entity.User;
+import codersbay.vienna.nachhilfe.wien.backend.model.updaterequest.AdminUpdateRequest;
+import codersbay.vienna.nachhilfe.wien.backend.model.updaterequest.StudentUpdateRequest;
+import codersbay.vienna.nachhilfe.wien.backend.rest.exceptions.UserNotFoundException;
 import codersbay.vienna.nachhilfe.wien.backend.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.LogManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/admin")
@@ -23,6 +29,22 @@ public class AdminController {
         return new ResponseEntity<>(adminList, HttpStatus.OK);
     }
 
-
-
+    @PutMapping("/updateAdmin/{adminId}")
+    public ResponseEntity<Admin> updateAdmin(
+            @PathVariable Long adminId,
+            @RequestBody AdminUpdateRequest request
+    ) {
+        Admin updatedAdmin =
+                adminService.updateAdmin (adminId,
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getDescription());
+        return new ResponseEntity<>(updatedAdmin, HttpStatus.OK);
+    }
 }
+
+
+
+
+
+
--- src/main/java/codersbay/vienna/nachhilfe/wien/backend/service/AdminService.java
+++ src/main/java/codersbay/vienna/nachhilfe/wien/backend/service/AdminService.java
@@ -1,11 +1,17 @@
 package codersbay.vienna.nachhilfe.wien.backend.service;
 
 import codersbay.vienna.nachhilfe.wien.backend.model.Entity.Admin;
+import codersbay.vienna.nachhilfe.wien.backend.model.Entity.Profile;
+import codersbay.vienna.nachhilfe.wien.backend.model.Entity.Student;
+import codersbay.vienna.nachhilfe.wien.backend.model.Entity.User;
 import codersbay.vienna.nachhilfe.wien.backend.respository.AdminRepository;
+import codersbay.vienna.nachhilfe.wien.backend.rest.exceptions.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.LogManager;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -15,5 +21,39 @@ public class AdminService {
 
     public List<Admin> findAllAdmins() {
         return adminRepository.findAll();
+    }
+
+    public Optional<Admin> findById(Long adminId) {
+        Optional<Admin> admin = adminRepository.findById(adminId);
+        if(admin.isPresent()){
+            return admin;
+        } else {
+            throw new UserNotFoundException("Admin not found");
+        }
+    }
+
+    public Admin updateAdmin(Long adminId, String firstName, String lastName, String description) {
+        Optional<Admin> admin  = adminRepository.findById(adminId);
+        if (admin.isPresent()) {
+            Admin existingAdmin = admin.get();
+
+            User user = existingAdmin.getProfile().getUser();
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+
+            Profile profile = existingAdmin.getProfile();
+
+            //Update the properties of the existing teacher with the updated values
+            profile.setActive(profile.isActive());
+            profile.setDescription(description);
+            profile.setPassword(profile.getPassword());
+            profile.setEmail(profile.getEmail());
+
+            adminRepository.save(existingAdmin);
+
+            return existingAdmin;
+        } else {
+            throw new UserNotFoundException("Admin not found");
+        }
     }
 }
