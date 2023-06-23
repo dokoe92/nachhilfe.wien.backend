891cb09841f5a31d9ada4e992e704fa0029c86b4
Clemens Pilz <office@clemenspilzcom>
2023-06-23 07:00:08 +0200

    fixed TeacherPublicMapper

--- src/main/java/codersbay/vienna/nachhilfe/wien/backend/mapper/teachermapper/TeacherPublicMapper.java
+++ src/main/java/codersbay/vienna/nachhilfe/wien/backend/mapper/teachermapper/TeacherPublicMapper.java
@@ -1,6 +1,7 @@
 package codersbay.vienna.nachhilfe.wien.backend.mapper.teachermapper;
 
 import codersbay.vienna.nachhilfe.wien.backend.dto.teacherdto.TeacherPublicDTO;
+import codersbay.vienna.nachhilfe.wien.backend.mapper.coachingmapper.CoachingMapper;
 import codersbay.vienna.nachhilfe.wien.backend.mapper.feedbackmapper.FeedbackMapper;
 import codersbay.vienna.nachhilfe.wien.backend.mapper.feedbackmapper.FeedbacksMapper;
 import codersbay.vienna.nachhilfe.wien.backend.model.Teacher;
@@ -15,14 +16,15 @@ public class TeacherPublicMapper {
 
     private final FeedbacksMapper feedbacksMapper;
     private final FeedbackMapper feedbackMapper;
+    private final CoachingMapper coachingMapper;
 
     public TeacherPublicDTO toDTO(Teacher teacher) {
         TeacherPublicDTO teacherPublicDTO = new TeacherPublicDTO();
         teacherPublicDTO.setTeacherId(teacher.getId());
         teacherPublicDTO.setFirstName(teacher.getFirstName());
-        teacherPublicDTO.setLastName(teacherPublicDTO.getLastName());
-        teacherPublicDTO.setDescription(teacherPublicDTO.getDescription());
-        teacherPublicDTO.setImage(teacherPublicDTO.getImage());
+        teacherPublicDTO.setLastName(teacher.getLastName());
+        teacherPublicDTO.setDescription(teacher.getDescription());
+        teacherPublicDTO.setImage(teacher.getProfile().getImageBase64());
         teacherPublicDTO.setActive(teacher.getProfile().isActive());
         teacherPublicDTO.setAverageRatingScore(teacher.getProfile().getAverageRatingScore());
         teacherPublicDTO.setFeedbacks(teacher.getFeedbacks()
@@ -30,6 +32,9 @@ public class TeacherPublicMapper {
                 .map(feedbackMapper::toDTO)
                 .collect(Collectors.toSet()));
         teacherPublicDTO.setDistricts(teacher.getDistricts());
+        teacherPublicDTO.setCoachings(teacher.getCoachings()
+                .stream().map(coachingMapper::toDTO)
+                .collect(Collectors.toSet()));
 
         return teacherPublicDTO;
     }
