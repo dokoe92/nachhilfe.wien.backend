d10a446ed64374ab9d4329d45ec72b7c12a82877
Clemens Pilz <office@clemenspilzcom>
2023-06-26 19:41:47 +0200

    added more checks

--- src/main/java/codersbay/vienna/nachhilfe/wien/backend/rest/UserController.java
+++ src/main/java/codersbay/vienna/nachhilfe/wien/backend/rest/UserController.java
@@ -1,13 +1,17 @@
 package codersbay.vienna.nachhilfe.wien.backend.rest;
 
+import codersbay.vienna.nachhilfe.wien.backend.config.security.JwtService;
 import codersbay.vienna.nachhilfe.wien.backend.dto.userdto.ProfileDTO;
 import codersbay.vienna.nachhilfe.wien.backend.mapper.usermapper.ProfileMapper;
 import codersbay.vienna.nachhilfe.wien.backend.model.User;
+import codersbay.vienna.nachhilfe.wien.backend.respository.ProfileRepository;
+import codersbay.vienna.nachhilfe.wien.backend.rest.exceptions.UserNotAuthorizedException;
 import codersbay.vienna.nachhilfe.wien.backend.rest.exceptions.UserNotFoundException;
 import codersbay.vienna.nachhilfe.wien.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +22,14 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
-@SecurityRequirement(name="bearerAuth")
+@SecurityRequirement(name = "bearerAuth")
 @Tag(name = "User")
 public class UserController {
 
     private final UserService userService;
     private final ProfileMapper profileMapper;
+    private final ProfileRepository profileRepository;
+    private final JwtService jwtService;
 
     @GetMapping("/{id}")
     @Operation(
@@ -38,24 +44,33 @@ public class UserController {
         }
     }
 
-@PostMapping("/picture/{id}")
+    @PostMapping("/picture/{id}")
     @Operation(
             summary = "Update a user's profile picture"
     )
-    public ResponseEntity<ProfileDTO> setProfilePicture(@PathVariable Long id, @RequestBody String imageBase64) {
+    public ResponseEntity<ProfileDTO> setProfilePicture(@PathVariable Long id,
+                                                        @RequestBody String imageBase64,
+                                                        HttpServletRequest request) {
+        if (imageBase64.length() > 3000000) {
+            throw new IllegalArgumentException("Image is too large");
+        }
+
         Optional<User> user = userService.findById(id);
-        if (user.isPresent()) {
-            user.get().getProfile().setImageBase64(imageBase64);
-            return new ResponseEntity<>(profileMapper.toDTO(user.get().getProfile()), HttpStatus.OK);
-        } else {
+        if (user.isEmpty()) {
             throw new UserNotFoundException("User not found");
         }
 
-}
+        User actualUser = user.get();
+        String token = jwtService.getTokenFromHeader(request.getHeader("Authorization"));
+        Long userId = jwtService.extractUserId(token);
+        if (!userId.equals(id)) {
+            throw new UserNotAuthorizedException("User not authorized!");
+        }
 
-
-
-
+        actualUser.getProfile().setImageBase64(imageBase64);
+        profileRepository.save(actualUser.getProfile());
+        return new ResponseEntity<>(profileMapper.toDTO(actualUser.getProfile()), HttpStatus.OK);
+    }
 
 
 }
