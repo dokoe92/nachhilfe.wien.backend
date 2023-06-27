de2a79c7d2a96ef064ec34d424b57bce6f252b64
Dominik Köberl <dokoe92@gmail.com>
2023-06-27 22:57:34 +0200

    added unit test testGetAllAppointments_UserWithTwoCoachings_ReturnAllAppointments

--- src/test/java/codersbay/vienna/nachhilfe/wien/backend/service/AppointmentServiceTest.java
+++ src/test/java/codersbay/vienna/nachhilfe/wien/backend/service/AppointmentServiceTest.java
@@ -61,9 +61,7 @@ class AppointmentServiceTest {
         Coaching coaching = new Coaching();
 
         Appointment appointment1 = new Appointment();
-        appointment1.setCoaching(coaching);
         Appointment appointment2 = new Appointment();
-        appointment2.setCoaching(coaching);
 
         coachings.add(coaching);
         appointments.add(appointment1);
@@ -74,7 +72,6 @@ class AppointmentServiceTest {
 
         AppointmentDTO appointmentDTO1 = new AppointmentDTO();
         AppointmentDTO appointmentDTO2 = new AppointmentDTO();
-        Set<AppointmentDTO> appointmentDTOS = new HashSet<>();
 
         when(userRepository.findById(userId)).thenReturn(Optional.of(student));
         when(appointmentMapper.toDTO(appointment1)).thenReturn(appointmentDTO1);
@@ -87,5 +84,52 @@ class AppointmentServiceTest {
         assertEquals(2, result.size());
     }
 
+    @Test
+    void testGetAllAppointments_UserWithTwoCoachings_ReturnAllAppointments() {
+        //given
+        Long userId = 1L;
+        Student student = new Student();
+        Set<Coaching> coachings = new HashSet<>();
+        Set<Appointment> appointmentsCoaching1 = new HashSet<>();
+        Set<Appointment> appointmentsCoaching2 = new HashSet<>();
+
+
+        Coaching coaching1 = new Coaching();
+        Coaching coaching2 = new Coaching();
+
+        Appointment appointment1 = new Appointment();
+        Appointment appointment2 = new Appointment();
+        Appointment appointment3 = new Appointment();
+        Appointment appointment4 = new Appointment();
+
+        coachings.add(coaching1);
+        coachings.add(coaching2);
+        appointmentsCoaching1.add(appointment1);
+        appointmentsCoaching1.add(appointment2);
+        appointmentsCoaching2.add(appointment3);
+        appointmentsCoaching2.add(appointment4);
+
+        student.setCoachings(coachings);
+        coaching1.setAppointments(appointmentsCoaching1);
+        coaching2.setAppointments(appointmentsCoaching2);
+
+
+        AppointmentDTO appointmentDTO1 = new AppointmentDTO();
+        AppointmentDTO appointmentDTO2 = new AppointmentDTO();
+        AppointmentDTO appointmentDTO3 = new AppointmentDTO();
+        AppointmentDTO appointmentDTO4 = new AppointmentDTO();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(student));
+        when(appointmentMapper.toDTO(appointment1)).thenReturn(appointmentDTO1);
+        when(appointmentMapper.toDTO(appointment2)).thenReturn(appointmentDTO2);
+        when(appointmentMapper.toDTO(appointment3)).thenReturn(appointmentDTO3);
+        when(appointmentMapper.toDTO(appointment4)).thenReturn(appointmentDTO4);
+
+        //when
+        Set<AppointmentDTO> result = underTest.getAllAppointments(userId);
+
+        //then
+        assertEquals(4, result.size());
+    }
 
 }
